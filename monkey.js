9ebf90f0291005f0b436d65276bbeb33a341f5d3
ngti21md <117225303+ngti21md@users.noreply.github.com>
2023-05-21 16:00:48 +0200

    new

--- monkey.js
+++ monkey.js
@@ -2,36 +2,30 @@ export class Monkey {
     constructor(x, y, monkeyLeftImg, monkeyRightImg) {
       this.x = x;
       this.y = y;
-      this.monkeyLeftImg = monkeyLeft;
-      this.monkeyRightImg = monkeyRight;
+      this.monkeyLeftImg = monkeyLeftImg;
+      this.monkeyRightImg = monkeyRightImg;
       this.image = this.monkeyRightImg;
     }
-    // draw() {
-    //   push();
-    //   translate(this.x, this.y);
-    //   fill(0, 200, 0);
-    //   rect(375, 400, 50);
-    //   pop();
-    // }
-
+  
     moveUp() {
-        this.y += -speed * 2;
+      this.y += -speed * 2;
     }
+  
     moveDown() {
-        this.y += +speed;
+      this.y += +speed;
     }
-    moveLeft () {
-        this.x += -speed - 1;
-        this.image = this.monkeyLeftImg;
+  
+    moveLeft() {
+      this.x += -speed - 1;
+      this.image = this.monkeyLeftImg;
     }
-    
+  
     moveRight() {
-        this.x += +speed + 1;
-        this.image = this.monkeyRightImg;
+      this.x += +speed + 1;
+      this.image = this.monkeyRightImg;
     }
-
+  
     display() {
-        this.image(this.image, this.x, this.y, 50, 50);
+      image(this.image, this.x, this.y, 50, 50);
     }
   }
-  
